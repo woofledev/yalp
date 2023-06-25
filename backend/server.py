9d4f39573c906f2fdb38e83c8fe865d133456cdf
woofle <83413563+woofledev@users.noreply.github.com>
2023-06-25 12:52:00 +0200

    multiple repositories, backend update.

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
-import os
+import os, logging
 from flask import Flask, send_from_directory, request
 
 app = Flask(__name__)
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/yalp/pkg/<path:path>')
 def send_package(path):
@@ -11,10 +12,10 @@ def send_package(path):
 def search_packages():
     q = request.args.get('q')
     files = os.listdir('packages')
-    matching = [f for f in files if not f.endswith('.config') and q in f]
-    resp = f'Found {len(matching)} packages/files for query "{q}"\n'
+    matching = [f.split('.config')[0] for f in files if f.endswith('.config') and q in f]
+    resp = f'Found {len(matching)} packages for query "{q}":\n'
     resp += '\n'.join(f' {f}' for f in matching)
     return resp+'\n'
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(host='0.0.0.0', port=80)
